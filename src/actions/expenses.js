1044364e8e99ccedc8c0131606766d51cbe1ef2a
Manish <manish.chandwaney@gmail.com>
2019-06-12 20:58:11 -0400

    Add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -45,6 +45,14 @@ id,
 updates
 });
 
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        })
+    };
+};
+
 export const setExpenses = (expenses) => ({
     type: 'SET_EXPENSES',
     expenses
